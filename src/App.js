9be1ea273b1086371758761618c67182a1007fa0
dobob777 <dobob777@gmail.com>
2023-07-07 16:53:51 +0530

    password task aisne

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [checkbox, setCheckbox] = useState(false)
+  console.log('checkbox::: ', checkbox);
+  // const [Name, setName] = useState('Dharam')
+  // const changeSetName = (e) => {
+  //   console.log('e::: ', e.target.value);
+  //   setName(e.target.value)
+  // }
+
+  // const changeSetRadio = (e) => {
+  //   console.log(e.target.value);
+  //   console.log(e.target.checked);
+  // }
+
+  // const changeSetChbox = (e) => {
+  //   console.log(e.target.value);
+  //   console.log(e.target.checked);
+  //   setCheckbox(!checkbox)
+  // }
   return (
-    <div>App</div>
+    <div>
+      {/* <input type="text" onChange={ changeSetName } value={ Name } /> */ }
+      {/* <input
+        name='gender'
+        type="radio"
+        onChange={ changeSetRadio }
+        value={ 'Male' } />Male
+      <input
+        name='gender'
+        type="radio"
+        onChange={ changeSetRadio }
+        value={ 'Female' }
+      />Female */}
+
+
+      {/* <input checked={ checkbox ? 'checked' : '' } onChange={ changeSetChbox } type="checkbox" value={ 'hello' } />Hello
+      <input onChange={ changeSetChbox } type="checkbox" value={ 'Ashish' } />Ashish
+      <input onChange={ changeSetChbox } type="checkbox" value={ 'Prashant' } />Prashant */}
+    </div>
   )
 }
 
