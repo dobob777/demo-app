51b8c9b0dd49e842405614f98007870b43f738f1
dobob777 <dobob777@gmail.com>
2023-07-17 17:12:12 +0530

    useEffect

--- src/comp/Table.jsx
+++ src/comp/Table.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Table = ({ name, email }) => {
-    // const { name, email } = props
+const Table = () => {
+
+    const [count, setCount] = useState([])
+    const [sahil, setSahil] = useState()
+    const [no, setNo] = useState(null)
+    const clickTo = () => {
+        const abc = {
+            name: '',
+            email: '',
+            dis: false
+        }
+        setNo(count.length)
+        setCount([...count, abc])
+    }
+    const clickTodis = () => {
+        // 
+        const abcd = count.filter((e, index) => {
+            return index == no
+        })
+        abcd[0].dis = true
+        abcd[0] = count[no]
+        console.log(abcd);
+        // setCount([...count, abc])
+    }
+
     return (
-        <div>
-            <label htmlFor="namer">Name:-
-                <p>{ name }</p>
-                <p>{ email }</p>
-            </label>
-        </div>
+        <>
+            <button onClick={ clickTo }>Add+</button>
+            {
+                count.map((ele, index) => {
+                    return (
+                        <tr key={ index }>
+                            <th>Name: <input disabled={ ele.dis ? "disabled" : null } type="text" /></th>
+                            <th>Email: <input disabled={ ele.dis ? "disabled" : null } type="text" /></th>
+
+                        </tr>
+                    )
+                })
+            }
+            <button onClick={ clickTodis }>save</button>
+        </>
     )
 }
 
